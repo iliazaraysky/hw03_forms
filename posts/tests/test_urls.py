6dc0841692b0c924ce1b2b90be82a07ac67ba140
Ilia Zaraysky <nextontext@yandex.ru>
2021-03-11 16:45:26 +0300

    Add tests Models, Urls, Views

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,10 +1,101 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
+
+from posts.models import Group, Post
+
+User = get_user_model()
 
 
 class StaticURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.group = Group.objects.create(
+            title='Лев Толстой',
+            slug='tolstoy',
+            description='Группа Льва Толстого',
+        )
+
+        cls.post = Post.objects.create(
+            group=StaticURLTests.group,
+            text="Какой то там текст",
+        )
+
     def setUp(self):
         self.guest_client = Client()
+        self.user = User.objects.create_user(username='TestForTest')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
-    def test_homepage(self):
+    def test_urls_list(self):
+        """
+        Тестирование по списку, для анонимного пользователя,
+        список URL, где ответ должен быть равен 200
+        """
+
+        url_list = ['/', '/group/tolstoy']
+        for test_url in url_list:
+            response = self.guest_client.get(test_url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_new_page_not_login_user(self):
+        """Страница доступна авторизированному пользователю"""
+
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_group_slug_response_not_login_user(self):
+        """Проверка доступности созданной группы для всех пользователей"""
+
+        response = self.guest_client.get('/group/tolstoy')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_page_not_login_user(self):
+        """
+        Страница создания нового поста,
+        перенаправляет анонимного пользователя
+        """
+
+        response = self.guest_client.get('/new')
+        self.assertEqual(response.status_code, 302)
+
+    def test_new_page_login_user(self):
+        """Главная страница доступна авторизированному пользователю"""
+
+        response = self.authorized_client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_group_slug_response_login_user(self):
+        """
+        Проверка доступности созданной группы
+        для авторизированных пользователей
+        """
+
+        response = self.authorized_client.get('/group/tolstoy')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_page_login_user(self):
+        """Страница доступна авторизированному пользователю"""
+
+        response = self.authorized_client.get('/new')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_page_not_login_user_redirect(self):
+        """Страница перенаправляет анонимного пользователя"""
+
+        response = self.guest_client.get('/new', follow=True)
+        self.assertRedirects(response, '/auth/login/?next=/new')
+
+    def test_urls_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон"""
+
+        template_url_names = {
+            'index.html': '/',
+            'group.html': '/group/tolstoy',
+            'newpost.html': '/new',
+        }
+        for template, reverse_name in template_url_names.items():
+            with self.subTest():
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
---
+++ posts/tests/test_views.py
@@ -0,0 +1,77 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+from django import forms
+
+from posts.models import Group, Post
+
+User = get_user_model()
+
+
+class StaticURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.group = Group.objects.create(
+            title='Лев Толстой',
+            slug='tolstoy',
+            description='Группа Льва Толстого',
+        )
+
+        cls.post = Post.objects.create(
+            group=StaticURLTests.group,
+            text="Какой-то там текст",
+        )
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.user = User.objects.create_user(username='TestForTest')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон"""
+
+        templates_page_names = {
+            'index.html': reverse('index'),
+            'newpost.html': reverse('new_post'),
+            'group.html': (reverse('group', kwargs={'slug': 'tolstoy'})),
+        }
+
+        for template, reverse_name in templates_page_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
+
+    def test_new_post_page_show_correct_context(self):
+        """Форма добавления материала сформирована с правильным контекстом"""
+        response = self.authorized_client.get(reverse('new_post'))
+        form_fields = {
+            'group': forms.fields.ChoiceField,
+            'text': forms.fields.CharField,
+        }
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_fields = response.context['form'].fields[value]
+                self.assertIsInstance(form_fields, expected)
+
+    def test_home_page_show_correct_context(self):
+        """Пост отображается на главной странице"""
+        response = self.authorized_client.get('/')
+        first_object = response.context['posts'][0]
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group.title
+        self.assertEqual(post_text_0, 'Какой-то там текст')
+        self.assertEqual(post_group_0, 'Лев Толстой')
+
+    def test_group_page_show_correct_context(self):
+        """Пост отображается на странице группы"""
+        response = self.authorized_client.get(
+            reverse('group', kwargs={'slug': 'tolstoy'}))
+        first_object = response.context['posts'][0]
+        post_text_0 = first_object.text
+        post_group_0 = first_object.group.title
+        self.assertEqual(post_text_0, 'Какой-то там текст')
+        self.assertEqual(post_group_0, 'Лев Толстой')
